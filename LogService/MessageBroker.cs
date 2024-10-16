f7c62ca51e18c7ab556c846d4bcb9189073f263c
Intermetheus <casperlykkerasmussen@gmail.com>
2024-10-16 12:49:57 +0200

    Received messages get written to TransactionLog.txt

--- LogService/MessageBroker.cs
+++ LogService/MessageBroker.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Connections;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.IO;
 using System.Text;
 
 namespace LogService
@@ -14,11 +16,14 @@ namespace LogService
 
         private IConnection connection;
 
+        private string docPath;
+
         public MessageBroker()
         {
             factory = new ConnectionFactory() { HostName = url, Port = 5672 };
             connection = factory.CreateConnection();
             CreateChannel("Log", ExchangeType.Topic, "Transaction");
+            docPath = Directory.GetCurrentDirectory();
         }
 
         public void CreateChannel(string exchangeName, string exchangeType, string routingKey)
@@ -30,9 +35,23 @@ namespace LogService
 
             channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: routingKey);
 
-            var properties = channel.CreateBasicProperties();
-            properties.Persistent = true;
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
 
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                // Write the string array to a file named "TransactionLog.txt".
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "TransactionLog.txt"), true))
+                {
+                    outputFile.WriteLine(message);
+                }
+
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
             this.channel = channel;
         }
     }
